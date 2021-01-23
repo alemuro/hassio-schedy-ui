820a3bf4dbdd51ff252a4660ec63441f33e20706
Aleix Murtra <alemurbcn@gmail.com>
2021-01-23 19:32:36 +0100

    Fix envvars

--- schedy-ui/run.sh
+++ schedy-ui/run.sh
@@ -2,6 +2,6 @@
 
 CONFIG_PATH=/data/options.json
 
-SCHEDY_CONF_FILE=$(jq --raw-output ".schedy_config_file" $CONFIG_PATH)
+export SCHEDY_CONF_FILE=$(jq --raw-output ".schedy_config_file" $CONFIG_PATH)
 
 yarn run start
